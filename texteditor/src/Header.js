90cecf79e4a426b8ff0aab007c30a61ff6d4f93a
kazimayaan <kazimayaan486@gmail.com>
2023-03-22 14:32:33 +0530

    filled the header

--- texteditor/src/Header.js
+++ texteditor/src/Header.js
@@ -1,1 +1,14 @@
 import React from "react"
+
+
+
+export default function Header(){
+    return (
+        <header>
+
+        <div className = "Header">
+            <h1>Text Editor</h1>
+        </div>
+        </header>
+    )
+}
