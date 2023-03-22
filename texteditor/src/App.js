2391b4a8ce0db97e30c72144b56c485de5d2ec22
kazimayaan <kazimayaan486@gmail.com>
2023-03-22 23:12:21 +0530

    created textarea

--- texteditor/src/App.js
+++ texteditor/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Header from "./Header"
-
+import Editor from "./Editor"
 function App() {
   return (
     <div className="App">
     <Header /> 
+    <Editor />
     </div>
   );
 }
---
+++ texteditor/src/Editor.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Editor() {
+  return (
+    <main>
+
+    <div contentEditable = "true" className='editor--div'  >
+      lolalalldsasijdfojn
+    </div>
+    </main>
+
+  )
+}
+
+export default Editor
