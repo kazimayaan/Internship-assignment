22af6ad3d86b61443dbabb56065c5783cf54e3ed
kazimayaan <kazimayaan486@gmail.com>
2023-03-24 17:09:08 +0530

    added some styling

--- texteditor/src/Editor.js
+++ texteditor/src/Editor.js
@@ -8,46 +8,34 @@ function Editor() {
   const [bold, setBold] = useState(false);
   const [italic, setItalic] = useState(false);
   const [selectedText, setSelectedText] = useState("");
-  
+
   const getSelectedText = () => {
     if (document.getSelection) {
       var lol = document.getSelection();
       alert(lol);
     }
   };
-  const boldClickHandler = () => {
-    const editor = document.getElementById("editor");
-    if (bold == true) {
-      editor.style.fontWeight = "300";
-      setBold(false);
-    } else {
-      editor.style.fontWeight = "bold";
-      setBold(true);
-    }
+  const handleBold = () => {
+    document.execCommand("bold", false, null);
   };
 
-  const italicClickHandler = () => {
-    const editor = document.getElementById("editor");
-    if (italic === true) {
-      editor.style.fontStyle = "normal";
-      setItalic(false);
-    } else {
-      editor.style.fontStyle = "italic";
-      setItalic(true);
-    }
+  const handleItalic = () => {
+    document.execCommand("italic", false, null);
   };
 
   return (
     <main>
-      <button type="button" onClick={boldClickHandler}>
-        <i class="fa-solid fa-bold"></i>
-      </button>
-      <button type="button" onClick={italicClickHandler}>
-        <i class="fa-solid fa-italic"></i>
-      </button>
-      <button onClick={getSelectedText}>lol</button>
+      <div className="buttons">
+        <button type="button" onClick={handleBold}>
+          <i class="fa-solid fa-bold"></i>
+        </button>
+        <button type="button" onClick={handleItalic}>
+          <i class="fa-solid fa-italic"></i>
+        </button>
+        <button onClick={getSelectedText}>AlertSelectedText</button>
+      </div>
       <div contentEditable="true" className="editor--div" id="editor">
-        lolalalldsasijdfojn
+        Your Text Here.
       </div>
     </main>
   );
